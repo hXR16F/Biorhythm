973524b09ee28e6f2f045167fc7df2864823eb12
hXR16F <hxr16f.ar@gmail.com>
2019-06-21 16:23:24 +0200

    Add files via upload

--- src/main.bat
+++ src/main.bat
@@ -3,3 +3,4 @@ copy settings.ini modules /Y >nul
 cd modules
 call biorhythm.bat
 if exist "display.exe" (call "display.exe") else (if exist "display.au3" call "display.au3")
+del /f /q "settings.ini" >nul
